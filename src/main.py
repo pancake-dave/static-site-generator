effeb887eaedb0b549914719465761a7dac052bc
Dawid Bialokryty <dawid.bialokryty@gmail.com>
2026-02-18 17:08:32 +0100

    Add delete_public and copy_content to main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
+import shutil, os
+
 from textnode import TextNode, TextType
 
 def main():
     node = TextNode("Dummy text", TextType.LINK, "www.placek.com")
-    print(node)
+    # print(node)
+
+def delete_public(path_public):
+    shutil.rmtree(path_public)
+    os.mkdir(path_public)
+
+def copy_content(path_public, path_static):
+    for item in os.listdir(path_static):
+        if os.path.isfile(path_static + item):
+            print(path_static + item + " is a file")
+        else:
+            print(path_static + item + " is a dir")
+            copy_content(path_public, path_static + item + "/")
+
 
 main()
+copy_content("../public/", "../static/")
 
-# thiiiiiiis
+
+# print(os.path.exists("../static/"))
